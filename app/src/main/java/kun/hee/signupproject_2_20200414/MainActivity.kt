9126607e8d033b70fc0256a195cbad4581eaac79
smlrkh1107 <krae7789230@gmail.com>
2020-04-14 21:02:41 +0900

    EditText 내용 변경 감지 이벤트 처리[왜안돼?]

--- app/src/main/java/kun/hee/signupproject_2_20200414/MainActivity.kt
+++ app/src/main/java/kun/hee/signupproject_2_20200414/MainActivity.kt
@@ -16,7 +16,30 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+        idEdt.addTextChangedListener(object : TextWatcher{ // alt + enter 해서 3가지 다 넣기
+            override fun afterTextChanged(s: Editable?) {
+                val inputVal = s.toString()
+                if (inputVal.length == 0){
+                    idStatusTxt.text = "최소 여섯글자 이상으로 해주세요."
+                }
+                else if (inputVal.length < 6){
+                    idStatusTxt.text = "길이가 너무 짧습니다."
+                }
+                else {
+                    idStatusTxt.text = "사용해도 좋은 아이디 입니다."
+                }
+            }
 
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+
+
+        })
     }
 
     override fun setValues() {
