fece555e4be04596e2cf0d3640605a9116921219
smlrkh1107 <krae7789230@gmail.com>
2020-04-14 20:56:43 +0900

    baseActivity 구성하고 MainActivity가 BaseActivity를 상속

--- app/src/main/java/kun/hee/signupproject_2_20200414/MainActivity.kt
+++ app/src/main/java/kun/hee/signupproject_2_20200414/MainActivity.kt
@@ -2,6 +2,10 @@ package kun.hee.signupproject_2_20200414
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 // 상속을 BaseActivity()로 받자. Alt+Enter하고 1번째,-> TODO 지우장
@@ -12,6 +16,7 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
     }
 
     override fun setValues() {
